84686d09583536867ac64c7fbc1c5189bae026a2
Anubhav Agnihotri <anubahvagnihotri@gmail.com>
2023-02-06 20:03:20 +0530

    title updated

--- login/del_franchise.php
+++ login/del_franchise.php
@@ -40,7 +40,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>delete feedback</title>
+    <title>delete franchise</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
